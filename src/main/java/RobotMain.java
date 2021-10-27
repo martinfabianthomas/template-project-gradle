64694e42f09b22526364e50196b57fd4509e61a4
Martin <martin.thomas@smail.inf.h-brs.de>
2021-10-27 16:15:54 +0200

    Solved Exercise 1
    currently not properly solvable without measuring tape

--- src/main/java/RobotMain.java
+++ src/main/java/RobotMain.java
@@ -1,3 +1,8 @@
+import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import ev3dev.sensors.ev3.EV3TouchSensor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.utility.Delay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,11 +24,47 @@ public class RobotMain {
     );
 
     public static void main(final String[] args) {
+        var motorLeft = new EV3LargeRegulatedMotor(MotorPort.B);
+        var motorRight = new EV3LargeRegulatedMotor(MotorPort.C);
+        var userInput = new EV3TouchSensor(SensorPort.S4);
+
+        int degreesPerSec = 360 / 2;
+        motorLeft.setSpeed(degreesPerSec);
+        motorRight.setSpeed(degreesPerSec);
+
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            log.info("Shutting down");
+            log.info("Stopping Motors");
+            motorLeft.stop();
+            motorRight.stop();
         }));
 
         log.info("Program start");
+        while(!userInput.isPressed()) {
+            Delay.msDelay(50);
+        }
+
+        // Measured times for 2m distance:
+        //                               normal | when on carpet
+        // One full rotation per second - 11.58 | 12.45
+        //          ... every 2 seconds - 23.14 | 24.73
+        //          ... every 4 seconds - 47.28 | 49.46
+
+        // Educated guess for cm -> sec conversion at speed 360 / 2:
+        float cmToSecFactor = 25.0f / 200.0f;
+
+        // Optional improvement
+        //cmToSecRatio *= 0.94;
+        // Optional improvement for carpet
+        //cmToSecRatio *= 0.98;
+
+        motorLeft.forward();
+        motorRight.forward();
+
+        // Move slightly less than 2m forward:
+        // Robot is approximately 20cm long (without measuring tape 10cm is a very conservative guess)
+        // We can safely subtract the 20cm mentioned in the exercise without getting to close to edge, since
+        // both robot length and speed underestimate how close the robot actually gets.
+        Delay.msDelay((int) ((200 - 20 - 10) * cmToSecFactor * 1000));
 
         System.exit(0);
     }
