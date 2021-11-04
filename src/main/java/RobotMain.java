342a9c2794e1416d8ca214b3ee3456c402dfe253
Martin <martin.thomas@smail.inf.h-brs.de>
2021-11-04 22:10:29 +0100

    Solved E3

--- src/main/java/RobotMain.java
+++ src/main/java/RobotMain.java
@@ -1,5 +1,11 @@
+import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import ev3dev.sensors.ev3.EV3GyroSensor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.utility.Delay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import solutionclass.Movement;
 
 import java.util.Map;
 
@@ -19,11 +25,53 @@ public class RobotMain {
     );
 
     public static void main(final String[] args) {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            log.info("Shutting down");
-        }));
+        // Creating a separate class is not necessary, but makes the code more readable.
+        var movement = new Movement(MotorPort.B, MotorPort.C);
+
+        // This part was given
+        var gyroSensor = new EV3GyroSensor(SensorPort.S3);
+        var gyroRotation = gyroSensor.getAngleMode();
+        var rotationMeasurement = new float[gyroRotation.sampleSize()];
+        // Calibration delay for the gyro.
+        Delay.msDelay(4000);
 
         log.info("Program start");
+
+        // As in exercise 1
+        movement.TwoMeterStraight();
+
+        // Turn according to gyro.
+        movement.turnLeft();
+        // The following code naturally turns the robot too much. There are two valid solutions:
+        // 1. finding an appropriate correction term through trial and error
+        // 2. correcting the robots movement on the way back (harder)
+        while (rotationMeasurement[0] < 180) {
+            gyroRotation.fetchSample(rotationMeasurement, 0);
+            Delay.msDelay(10);
+        }
+        // This limits the amount by which it overshoots the 180 deg.
+        movement.bothStop();
+        Delay.msDelay(500);
+
+        // 2. Solution: Similar behaviour to movement.TwoMeterStraight, but once per second the robot might turn
+        // to correct its angle.
+        for (int sec = 0; sec < movement.secFor2m; sec++) {
+            movement.bothForward();
+            Delay.msDelay(1000);
+
+            gyroRotation.fetchSample(rotationMeasurement, 0);
+            if (rotationMeasurement[0] < 180) {
+                movement.turnLeft();
+            } else if (rotationMeasurement[0] > 180) {
+                movement.turnRight();
+            } else {
+                continue;
+            }
+
+            Delay.msDelay(100);
+            movement.bothStop();
+            Delay.msDelay(100);
+        }
 
         System.exit(0);
     }
---
+++ src/main/java/solutionclass/Movement.java
@@ -0,0 +1,67 @@
+package solutionclass;
+
+import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import lejos.hardware.port.Port;
+import lejos.utility.Delay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Movement {
+
+    private static final Logger log = LoggerFactory.getLogger(Movement.class);
+
+    public final EV3LargeRegulatedMotor motorLeft;
+    public final EV3LargeRegulatedMotor motorRight;
+
+    public final int defaultDegreesPerSec;
+    public final float secFor180deg;
+    public final int secFor2m;
+
+    public Movement(Port leftPort, Port rightPort) {
+        // All copied from last exercises RobotMain.main()
+        motorLeft = new EV3LargeRegulatedMotor(leftPort);
+        motorRight = new EV3LargeRegulatedMotor(rightPort);
+        defaultDegreesPerSec = 360 / 2;
+        secFor180deg = 0.97f * (41.25f / 20.0f);
+        secFor2m = 21;
+
+        motorLeft.setSpeed(defaultDegreesPerSec);
+        motorRight.setSpeed(defaultDegreesPerSec);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("Stopping movement motors");
+            motorLeft.stop();
+            motorRight.stop();
+        }));
+    }
+
+    public void bothForward() {
+        motorLeft.forward();
+        motorRight.forward();
+    }
+
+    public void bothStop() {
+        motorLeft.stop();
+        motorRight.stop();
+    }
+
+    public void turnLeft() {
+        motorLeft.forward();
+        motorRight.backward();
+    }
+
+    public void turnRight() {
+        motorLeft.backward();
+        motorRight.forward();
+    }
+
+    /**
+     * Solution to exercise 1
+     */
+    public void TwoMeterStraight() {
+        bothForward();
+        Delay.msDelay(secFor2m * 1000);
+        bothStop();
+        Delay.msDelay(500);
+    }
+}
