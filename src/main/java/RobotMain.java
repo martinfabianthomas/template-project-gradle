b999b32ffbc1e6b649969bd08ff961da5a9817cb
Martin <martin.thomas@smail.inf.h-brs.de>
2022-02-11 18:05:55 +0100

    finalized solution for real board

--- src/main/java/RobotMain.java
+++ src/main/java/RobotMain.java
@@ -1,8 +1,5 @@
 import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
-import ev3dev.sensors.Battery;
-import ev3dev.sensors.ev3.EV3TouchSensor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.SensorPort;
 import lejos.utility.Delay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,21 +10,11 @@ public class RobotMain {
 
     private static final Logger log = LoggerFactory.getLogger(RobotMain.class);
 
-    private static final Map<Integer, String> colorNames = Map.of(
-            0, "none",
-            1, "black",
-            2, "blue",
-            3, "green",
-            4, "yellow",
-            5, "red",
-            6, "white",
-            7, "brown"
-    );
-
     public static void main(final String[] args) {
         var motorLeft = new EV3LargeRegulatedMotor(MotorPort.B);
         var motorRight = new EV3LargeRegulatedMotor(MotorPort.C);
 
+        // Separate speeds for driving and turning can be used, but are not needed.
         int degreesPerSec = 360 / 2;
         motorLeft.setSpeed(degreesPerSec);
         motorRight.setSpeed(degreesPerSec);
@@ -40,24 +27,31 @@ public class RobotMain {
 
         log.info("Program start");
 
-        // The robot is positioned an the very start of the 2m and is moved forward until its tip is approx. 5cm from
-        // the end of the 2m. Times measured should be:
-        // One full rotation per second - 10s - 11s
-        //          ... every 2 seconds - 20s - 22s
-        //          ... every 4 seconds - 38s - 42s
-        //          ... every 8 seconds - 72s - 76s
-        // (on carpet about 4% - 5% more time, but the board does not have carpet)
+        // The robot turns one wheel forward and its other backward. Measure one 180 deg turn or multiple 360 deg
+        // turns and divide measured time accordingly. The times measured per 180 deg rotation should be about:
+        // One full rotation per second - 1s
+        //          ... every 2 seconds - 2s
+        //          ... every 4 seconds - 4s
+        //          ... every 8 seconds - 8.5s
 
-        // Value for motor speed of 360/2 deg/s
-        int secFor2m = 21;
+        // This value represents the measurement of 41.25 seconds for 20 180 deg turns.
+        float secFor180deg = 41.25f / 20.0f;
 
+        // From last exercise:
+        int secFor2m = 20;
+
+        // As in exercise 1
         motorLeft.forward();
         motorRight.forward();
-
         Delay.msDelay(secFor2m * 1000);
 
-        motorLeft.stop();
-        motorRight.stop();
+        // Turn as per measurement.
+        motorRight.backward();
+        Delay.msDelay((int) (secFor180deg * 1000));
+
+        // Drive back as in exercise 1
+        motorRight.forward();
+        Delay.msDelay(secFor2m * 1000);
 
         System.exit(0);
     }
