705869bce5b895d7a73f08b28e09f3d0adcf05b9
Martin <martin.thomas@smail.inf.h-brs.de>
2022-02-14 13:11:02 +0100

    Removed unneeded color map

--- src/main/java/RobotMain.java
+++ src/main/java/RobotMain.java
@@ -1,22 +1,9 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
-
 public class RobotMain {
 
     private static final Logger log = LoggerFactory.getLogger(RobotMain.class);
-
-    private static final Map<Integer, String> colorNames = Map.of(
-            0, "none",
-            1, "black",
-            2, "blue",
-            3, "green",
-            4, "yellow",
-            5, "red",
-            6, "white",
-            7, "brown"
-    );
 
     public static void main(final String[] args) {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
